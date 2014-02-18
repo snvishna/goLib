f9979d699e8c47829efaf42133e6a9113083f292
Shankar <vrsndgr8@gmail.com>
2014-02-18 00:01:07 -0700

    Added a string util function that reverses a string input
    My first checkin into Github: Trying to create and maintain a branch
    that contains some general purpose library functions written in Go.

---
+++ stringUtil/string_test.go
@@ -0,0 +1,19 @@
+package stringUtil
+
+import "testing"
+
+func Test(t *testing.T) {
+	var tests = []struct {
+		s, want string
+	} {
+		{"Backward", "drawkcaB"},
+		{"Hello, 世界", "界世 ,olleH"},
+		{"", ""},
+	}
+	for _, c := range tests {
+		got := Reverse(c.s)
+		if got != c.want {
+			t.Error("Reverse(%q) == %q, want %q,", c.s, got, c.want)
+		}
+	}
+}
